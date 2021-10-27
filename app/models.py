8bcd19d861c93b8d120003e907220c2554a667d2
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-28 01:36:01 +0300

    NeighbourHood model defined

--- app/models.py
+++ app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from datetime import datetime as dt
+from django.contrib.auth.models import User
 
-# Create your models here.
+# cloudinary
+from cloudinary.models import CloudinaryField
+
+
+# NeighbourHood Model
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def create_neigborhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def update_neighbourhood(cls, id):
+        cls.objects.filter(id=id).update()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    # find neighbourhood by id
+    @classmethod
+    def find_neigborhood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
