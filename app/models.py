08315b421007661868f6b41c4d3e036bda49e270
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-28 02:00:03 +0300

    cotact model defined

--- app/models.py
+++ app/models.py
@@ -59,6 +59,7 @@ class User(models.Model):
     password = models.CharField(max_length=50)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE)
+    profile_pic = CloudinaryField('image')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -99,6 +100,43 @@ class Business(models.Model):
     def find_business(cls, id):
         business = cls.objects.get(id=id)
         return business
+
+    def __str__(self):
+        return self.name
+
+
+# contact class model
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # create contact
+    def create_contact(self):
+        self.save()
+
+    # delete contact
+    def delete_contact(self):
+        self.delete()
+
+    # update contact
+    def update_contact(self):
+        self.update()
+
+    # search contact
+    @classmethod
+    def search_by_name(cls, search_term):
+        contact = cls.objects.filter(name__icontains=search_term)
+        return contact
+
+    # find contact by id
+    @classmethod
+    def find_contact(cls, id):
+        contact = cls.objects.get(id=id)
+        return contact
 
     def __str__(self):
         return self.name
