b442a154cf2966bf77e73ffe47ce03a01dee6bc0
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-30 23:14:48 +0300

    profile page set'

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("profile/", views.profile, name="profile"), # profile page
 ]
 
 
--- app/views.py
+++ app/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-
-
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 
 # index view
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'index.html')
+
+
+# profile view
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    user = request.user
+    profile = User.objects.get(user=user)
+    posts = Post.objects.filter(user=user)
+    return render(request, 'profile.html', {'profile': profile, 'posts': posts})
